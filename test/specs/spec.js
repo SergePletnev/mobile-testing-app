076b276b62b10dc13cc2b2030a99370400cfe49b
Siarhei Plistniou <siarhei_pliatniou@epam.com>
2018-08-20 02:19:46 +0300

    dev base tests

--- test/specs/spec.js
+++ test/specs/spec.js
@@ -1,17 +1,33 @@
 'use strict'
 
-describe('Simple test', () => {
-    // beforeAll(() => {
-    //     browser.get(baseUri);
-    // });
+function addTask(title, description) {
+    $('#addTaskButton').click();
+    $('#taskTitleEditText').setValue(title);
+    $('#taskDescriptionEditText').setValue(description);
+    browser.hideDeviceKeyboard();
+    $('#saveTaskButton').click();
+}
 
-    it('should have correct title', () => {
-        console.log('App test works!!!')
-        browser.debug();
+function modifyTask(newTitle, newDescription) {
+    $$('#text1')[0].click();
+    $('#taskTitleEditText').setValue(newTitle);
+    $('#taskDescriptionEditText').setValue(newDescription);
+    browser.hideDeviceKeyboard();
+    $('#saveTaskButton').click();
+}
+
+describe('Base app functinality', () => {
+
+    it('should be possible to add new task', () => {
+        addTask('New Task', 'New Description');
+        const newTask = $$('#text1')[3];
+        expect(newTask.getText()).toEqual('New Task');
     });
 
+    it('should be possible to modify task description', () => {
+        modifyTask('Modified Task', 'Modified Description');
+        const modifiedTask = browser.element('android= new UiSelector().resourceId("android:id/text1").instance(0)');
+        expect(modifiedTask.getText()).toEqual('Modified Task');
+    });
 
-    // afterAll(() => {
-    //     browser.driver.quit();
-    // });
 });
